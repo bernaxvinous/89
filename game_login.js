379086c3053ae1729b56e29f11ae037da5c35f01
bernaxvinous <150352167+bernaxvinous@users.noreply.github.com>
2024-04-04 14:22:11 -0300

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,9 +1,9 @@
 function addUser(){
-    player1Name =document.getElementById("player1NameInput").value;
-    player2Name =document.getElementById("player2NameInput").value;
+    player1_name =document.getElementById("player1NameInput").value;
+    player2_name =document.getElementById("player2NameInput").value;
 
-    localStorage.setItem("player1Name", player1Name);
-    localStorage.setItem("player2Name", player2Name);
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
 
     window.location = "game_page.html";
     
--- game_page.js
+++ game_page.js
@@ -8,7 +8,7 @@ document.getElementById("player1_name").innerHTML = player1_name + ":";
 document.getElementById("player2_name").innerHTML = player2_name + ":";
 
 document.getElementById("player1_score").innerHTML = player1_score ;
-document.getElementById("player1_score").innerHTML = player1_score ;
+document.getElementById("player2_score").innerHTML = player2_score ;
 
 document.getElementById("player_question").innerHTML = "turno de pergunta - "+ player1_name;
 document.getElementById("player_answer").innerHTML = "turno de resposta - "+ player2_name;
@@ -22,35 +22,38 @@ function send() {
     charAt1 = word.charAt(1);
     console.log(charAt1);
 
-    length_divide_2 = math.floor(word.length/2);
+    length_divide_2 = Math.floor(word.length/2);
     charAt2 = word.charAt(length_divide_2);
-    console.log(charAt2)
+    console.log(charAt2);
 
     length_minus_1 = word.length - 1;
     charAt3 = word.charAt(length_minus_1);
-console.log(charAt3)
+console.log(charAt3);
 
 remove_charAt1 = word.replace(charAt1, "_");
-remove_charAt2 = remove_charAt2.replace(charAt2, "_")
-remove_charAt3 = remove_charAt3.replace(charAt3, "_")
+remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+remove_charAt3 = remove_charAt2.replace(charAt3, "_");
 console.log(remove_charAt3);
 
 question_word = "<h4 id='word_display> p. "+remove_charAt3+"</h4>";
-input_box = "<br>resposta ; <input type='text' id='input_chek_box'>";                        
+input_box = "<br>resposta : <input type='text' id='input_check_box'>";                        
 check_button = "<br><br><button class='btn btn-info' onclick='check()'>Checar</button>";
 row = question_word + input_box + check_button ;
 document.getElementById("output").innerHTML = row;
 document.getElementById("word").value ="";
 }
 
+questionTurn ="player1";
+answerTurn ="player"
+
 function check()
 {
     getAnswer = document.getElementById("inputCheckBox").value;
-    answer = getAnswer.tolowerCase();
+    answer = getAnswer.toLowerCase();
     console.log("resposta em caixa baixa - "+ answer);
     if(answer == word)
     {
-        if(answerTurn == "player")
+        if(answerTurn == "player1")
         {
             player1_score = player1_score +1;
             document.getElementById("player1Score").innerHTML = player1Score;    
